59d5ea4df0cb0a58011a66bd9a9e9b410584823b
sommon4 <51230951+sommon4@users.noreply.github.com>
2024-06-20 12:22:08 +0700

    Create background.js

---
+++ chrome_extension/background.js
@@ -0,0 +1,8 @@
+chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+  const url = item.finalUrl;
+  if (url.includes('/outputs/VariantCallerActor-00/merged.bam.ptrim.bam')) {
+    const parts = url.split('/');
+    const filename = parts[parts.length - 5]; // Extract the sample name
+    suggest({ filename: filename + '.bam' });
+  }
+});
